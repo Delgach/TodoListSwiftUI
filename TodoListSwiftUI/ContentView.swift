cb7b31633d42a6f4791357b847e06676af06cd25
Vladimir D <vladimir_rds@icloud.com>
2021-12-08 15:17:28 +0300

    Initial Commit

---
+++ TodoListSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoListSwiftUI
+//
+//  Created by Владимир Дельгадильо on 08.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoListSwiftUI/TodoListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSwiftUIApp.swift
+//  TodoListSwiftUI
+//
+//  Created by Владимир Дельгадильо on 08.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
