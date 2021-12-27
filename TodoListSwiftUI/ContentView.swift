0cad6cbf16dbd041ae92e8ec01a306cfbf8f4dbc
Vladimir D <vladimir_rds@icloud.com>
2021-12-27 20:16:00 +0300

    saving list items

--- TodoListSwiftUI/ContentView.swift
+++ TodoListSwiftUI/ContentView.swift
@@ -15,21 +15,27 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach (list.items) { item in
-                    Text(item.name)
+                ForEach (list.items.indexed(), id: \.1.id) { index, book in
+                    HStack {
+                        Toggle(list.items[index].name, isOn: $list.items[index].isDone)
+                    }
                 }.onDelete(perform: list.removeItem).onMove(perform: list.moveItem)
             }.navigationBarItems(leading: Button(action: { self.isModalOpen = true }) {
                 Text("add new item")
-            }, trailing: EditButton())
+            }, trailing: EditButton()).onAppear{ list.saveData()}
             .sheet(isPresented: $isModalOpen) {
                 ItemForm(itemslist: self.list)
             }
         }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
+
--- TodoListSwiftUI/viewModals/ItemList.swift
+++ TodoListSwiftUI/viewModals/ItemList.swift
@@ -10,15 +10,56 @@ import Foundation
 class ItemsList: ObservableObject {
     @Published var items: [Item] = [Item(name: "name"), Item(name: "name2"), Item(name: "name3")]
     
+    init() {
+        loadData()
+    }
+    
     func removeItem(indexSet: IndexSet) {
         items.remove(atOffsets: indexSet)
+        saveData()
     }
     
     func moveItem(from: IndexSet, to: Int) {
         items.move(fromOffsets: from, toOffset: to)
+        saveData()
     }
     
     func addNewItem(name: String) {
         items.append(Item(name: name))
+        saveData()
+    }
+    
+    func getDocumentDirectory() -> URL {
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return documentDirectory[0]
+    }
+    
+    func getDataFilePath() -> URL {
+        return getDocumentDirectory().appendingPathComponent("TodoList.plist")
+    }
+    
+    func saveData() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let encodedData = try encoder.encode(items)
+            try encodedData.write(to: getDataFilePath())
+        }
+        catch {
+            print("throw error at saving: \(error.localizedDescription)")
+        }
+    }
+    
+    func loadData() {
+        let decoder = PropertyListDecoder()
+        
+        if let data = try? Data(contentsOf: getDataFilePath()) {
+            do {
+                items = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("throw error at load: \(error.localizedDescription)")
+            }
+        }
     }
 }
